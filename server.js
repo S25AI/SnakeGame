5b70daa1f9b8de4309aadbf0d34efb15bc220abe
S25AI <jade25st@gmail.com>
2017-03-14 00:59:56 +0300

    save users and scores in db

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const PORT = 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const app = express();
 
 const jsonParser = bodyParser.json();
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
@@ -28,6 +30,34 @@ app.get('/scores', (req, res) => {
 app.post('/scores', jsonParser, (req, res) => {
 	fs.writeFileSync('./scores.json', JSON.stringify(req.body));
 	res.send('data has been written');
+});
+
+app.get('/auth', (req, res) => {
+	let {login} = req.cookies;
+	let auth = !!login;
+	res.send(JSON.stringify({auth, login}));
+});
+
+app.post('/auth', jsonParser, (req, res) => {
+	let userData = fs.readFileSync('./users.json', 'utf-8');
+	let auth = false;
+	try {
+		if (!userData) {
+			userData = [];
+		} else  {
+			userData = JSON.parse(userData);
+		}
+
+		if (!userData.some(user => user == req.body[0]) || !userData.length) {
+			userData.push(req.body[0]);
+			fs.writeFileSync('./users.json', JSON.stringify(userData));
+			auth = true;
+			res.cookie('login', req.body[0], {maxAge: 86400000, httpOnly: true});
+		}
+		res.send(JSON.stringify({auth}));
+	} catch(e) {
+		console.log(e.message);
+	}
 });
 
 app.listen(PORT, () => {
